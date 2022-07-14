4279f77c69f77d4310fd69a2e692c7f1114c2ebf
Iker Abadia <iker.abadia@bosonit.com>
2022-07-14 16:32:28 +0200

    update

---
+++ ts-intro/src/ejercicios/04-tarea-tipado.ts
@@ -0,0 +1,29 @@
+
+interface Direccion{
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface SuperHeroe{
+    nombre: string;
+    edad: number;
+    direccion: Direccion;
+    mostrarDireccion: () => string;
+}
+
+const superHeroe: SuperHeroe = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion:{
+        calle: 'Main St',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    mostrarDireccion(){
+        return this.nombre + ', ' +this.direccion.calle + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais;
+    }
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
---
+++ ts-intro/src/index.ts
@@ -0,0 +1,32 @@
+ 
+ interface Reproductor{
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles;
+ }
+
+ interface Detalles{
+     autor: string;
+     anio: number;
+ }
+
+ const reproductor: Reproductor = {
+     volumen: 90,
+     segundo: 36,
+     cancion: 'Mess',
+     detalles: {
+         autor: 'Ed Sheeran',
+         anio: 2015
+     }
+ }
+
+ const autor = 'Fulano';
+
+ const{ volumen, segundo, cancion, detalles:{ autor: autorDetalle } } = reproductor; //Forma 1
+ //const {autor} = detalles;                                                         //Forma 2
+
+ console.log('Volumen: '+volumen);
+ console.log('Segundo: '+segundo);
+ console.log('Cancion: '+cancion);
+ console.log('Autor: '+autorDetalle);
