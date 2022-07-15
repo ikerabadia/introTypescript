a6ddd7d221bfe0c7e0708794bcd89efb6bd6c596
Iker Abadia <iker.abadia@bosonit.com>
2022-07-15 14:04:49 +0200

    update
    update

--- ts-intro/src/ejercicios/06-desestructuracion-funcion.ts
+++ ts-intro/src/ejercicios/06-desestructuracion-funcion.ts
@@ -1,6 +1,6 @@
 
 
-interface Producto {
+export interface Producto {
     desc: string;
     precio: number;
 }
@@ -15,7 +15,7 @@ const tableta: Producto = {
     precio: 350
 }
 
-function calculaISV(productos: Producto[]):[number, number] {
+export function calculaISV(productos: Producto[]):[number, number] {
     
     let total = 0;
 
